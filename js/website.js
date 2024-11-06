c044b9312c2526c53f9888be115e6af93ba2be10
zoeythomas39 <zoeywillowthomas@gmail.com>
2024-11-06 13:18:54 -0500

    more images

--- js/website.js
+++ js/website.js
@@ -1,1 +1,20 @@
-let catImage = document.querySelector('#cat')
+let catImage = document.querySelector('#cat');
+let ramonaImage = document.querySelector('#ramona');
+let maxineImage = document.querySelector('#maxine');
+
+ramonaImage.style.display = 'none';
+
+maxineImage.style.display = 'none';
+
+catImage.onclick = () => {
+    if(catImage.getAttribute('src') === 'images/cat_and_mouse.jpeg')
+    {
+        catImage.setAttribute('src', 'images/ramona.jpeg');
+
+    } else if(catImage.getAttribute('src') === 'images/ramona.jpeg')
+    {
+        catImage.setAttribute('src','images/maxine.jpeg');
+    } else {
+        catImage.setAttribute('src','images/cat_and_mouse.jpeg');
+    }
+}
