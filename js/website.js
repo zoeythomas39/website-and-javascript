dcefc60ebac10709a37bf4d41e19bf11fedf271e
zoeythomas39 <zoeywillowthomas@gmail.com>
2024-11-07 11:59:23 -0500

    updates thursday morning

--- js/website.js
+++ js/website.js
@@ -1,20 +1,32 @@
 let catImage = document.querySelector('#cat');
 let ramonaImage = document.querySelector('#ramona');
 let maxineImage = document.querySelector('#maxine');
+let pumpkinImage = document.querySelector('#pumpkin');
+let marieImage = document.querySelector('#marie');
 
 ramonaImage.style.display = 'none';
 
 maxineImage.style.display = 'none';
 
-catImage.onclick = () => {
-    if(catImage.getAttribute('src') === 'images/cat_and_mouse.jpeg')
-    {
-        catImage.setAttribute('src', 'images/ramona.jpeg');
+marieImage.style.display = 'none';
 
-    } else if(catImage.getAttribute('src') === 'images/ramona.jpeg')
+catImage.onclick = () => {
+    if(catImage.getAttribute('src') === 'images/cat_and_mouse_resized.jpg')
     {
-        catImage.setAttribute('src','images/maxine.jpeg');
+        catImage.setAttribute('src', 'images/ramona_resized.jpg');
+
+    } else if(catImage.getAttribute('src') === 'images/ramona_resized.jpg')
+    {
+        catImage.setAttribute('src','images/maxine_resized.jpg');
     } else {
-        catImage.setAttribute('src','images/cat_and_mouse.jpeg');
+        catImage.setAttribute('src','images/cat_and_mouse_resized.jpg');
     }
-}
+};
+
+pumpkinImage.onmouseover = () => {
+pumpkinImage.setAttribute('src', 'images/marie_kitty_resized.jpg')
+};
+
+pumpkinImage.onmouseout = () => {
+    pumpkinImage.setAttribute('src', 'images/pumpkin.svg')
+};
